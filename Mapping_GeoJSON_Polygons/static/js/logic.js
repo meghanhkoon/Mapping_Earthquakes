d5fc3b0402c4b2e1653295353a6ac4fe1c262dcf
Meghan Koon <meghankoon@Meghans-MacBook-Air.local>
2022-11-21 11:42:58 -1000

    adding lines

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -32,7 +32,7 @@ L.control.layers(baseMaps).addTo(map);
 let torontoHoods = "https://raw.githubusercontent.com/meghanhkoon/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
